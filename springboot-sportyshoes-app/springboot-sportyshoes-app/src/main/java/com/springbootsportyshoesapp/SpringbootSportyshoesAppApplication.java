831ce5edfa1e9ea3f53d834e96fb3e841e9f0ec3
Swathi Suheal <swathi.suheal@gmail.com>
2021-11-11 22:21:43 +0530

    1st Commit

---
+++ springboot-sportyshoes-app/springboot-sportyshoes-app/src/main/java/com/springbootsportyshoesapp/SpringbootSportyshoesAppApplication.java
@@ -0,0 +1,13 @@
+package com.springbootsportyshoesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSportyshoesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSportyshoesAppApplication.class, args);
+	}
+
+}
---
+++ springboot-sportyshoes-app/springboot-sportyshoes-app/src/test/java/com/springbootsportyshoesapp/SpringbootSportyshoesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootsportyshoesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSportyshoesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
